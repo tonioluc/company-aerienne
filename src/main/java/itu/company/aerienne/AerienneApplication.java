ba0ce5ef435d60b076b8c9add1d92b0055695df3
antonio <antoniorandria3342@gmail.com>
2026-01-08 20:23:52 +0300

    Initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/itu/company/aerienne/AerienneApplication.java
@@ -0,0 +1,13 @@
+package itu.company.aerienne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerienneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerienneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itu/company/aerienne/AerienneApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.company.aerienne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerienneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
