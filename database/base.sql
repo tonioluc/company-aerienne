cd7e0a5b39f92f4f031335f9435be60ed6013fc1
antonio <antoniorandria3342@gmail.com>
2026-01-09 14:39:14 +0300

    Refactor database schema by creating tables for aeroport, avion, modele_vol, vol, and vol_avion

--- database/base.sql
+++ database/base.sql
@@ -5,8 +5,44 @@ drop database if exists aerienne;
 create database aerienne;
 \c aerienne;
 
-create table avions (
-    id serial primary key,
-    modele varchar(50) not null,
-    capacite int not null
+CREATE TABLE aeroport(
+   Id_aeroport SERIAL,
+   code_IATA VARCHAR(50) ,
+   PRIMARY KEY(Id_aeroport)
+);
+
+CREATE TABLE avion(
+   Id_avion SERIAL,
+   capacité NUMERIC(15,2)  ,
+   modele VARCHAR(50) ,
+   PRIMARY KEY(Id_avion)
+);
+
+CREATE TABLE modele_vol(
+   Id_modele_vol SERIAL,
+   numero_vol VARCHAR(50) ,
+   Id_aeroport INTEGER NOT NULL,
+   Id_aeroport_1 INTEGER NOT NULL,
+   PRIMARY KEY(Id_modele_vol),
+   FOREIGN KEY(Id_aeroport) REFERENCES aeroport(Id_aeroport),
+   FOREIGN KEY(Id_aeroport_1) REFERENCES aeroport(Id_aeroport)
+);
+
+CREATE TABLE vol(
+   Id_vol SERIAL,
+   date_heure_depart TIMESTAMP,
+   date_heure_arrive TIMESTAMP,
+   Id_modele_vol INTEGER NOT NULL,
+   PRIMARY KEY(Id_vol),
+   FOREIGN KEY(Id_modele_vol) REFERENCES modele_vol(Id_modele_vol)
+);
+
+CREATE TABLE vol_avion(
+   Id_avion INTEGER,
+   Id_vol INTEGER,
+   date_heure_depart TIMESTAMP,
+   date_heure_arrive TIMESTAMP,
+   PRIMARY KEY(Id_avion, Id_vol),
+   FOREIGN KEY(Id_avion) REFERENCES avion(Id_avion),
+   FOREIGN KEY(Id_vol) REFERENCES vol(Id_vol)
 );
