5968a0cdb4987d7d00d8e5eeeca0fdb8f17cc7db
antonio <antoniorandria3342@gmail.com>
2026-01-09 18:52:47 +0300

    Define new database schema and sample data for airline application

--- database/base.sql
+++ database/base.sql
@@ -7,7 +7,8 @@ create database aerienne;
 
 CREATE TABLE aeroport(
    Id_aeroport SERIAL,
-   code_IATA VARCHAR(50) ,
+   code_IATA VARCHAR(50),
+   ville VARCHAR(100),
    PRIMARY KEY(Id_aeroport)
 );
 
@@ -21,28 +22,60 @@ CREATE TABLE avion(
 CREATE TABLE modele_vol(
    Id_modele_vol SERIAL,
    numero_vol VARCHAR(50) ,
-   Id_aeroport INTEGER NOT NULL,
-   Id_aeroport_1 INTEGER NOT NULL,
+   Id_aeroport_depart INTEGER NOT NULL,
+   Id_aeroport_arrive INTEGER NOT NULL,
    PRIMARY KEY(Id_modele_vol),
-   FOREIGN KEY(Id_aeroport) REFERENCES aeroport(Id_aeroport),
-   FOREIGN KEY(Id_aeroport_1) REFERENCES aeroport(Id_aeroport)
+   FOREIGN KEY(Id_aeroport_depart) REFERENCES aeroport(Id_aeroport),
+   FOREIGN KEY(Id_aeroport_arrive) REFERENCES aeroport(Id_aeroport)
 );
 
 CREATE TABLE vol(
    Id_vol SERIAL,
    date_heure_depart TIMESTAMP,
    date_heure_arrive TIMESTAMP,
+   prix_place NUMERIC(15,2) DEFAULT 0,
    Id_modele_vol INTEGER NOT NULL,
    PRIMARY KEY(Id_vol),
    FOREIGN KEY(Id_modele_vol) REFERENCES modele_vol(Id_modele_vol)
 );
 
+CREATE TABLE achat_places(
+   Id_achat_places SERIAL,
+   nom_client VARCHAR(50) ,
+   nombre_place VARCHAR(50) ,
+   Id_vol INTEGER NOT NULL,
+   PRIMARY KEY(Id_achat_places),
+   FOREIGN KEY(Id_vol) REFERENCES vol(Id_vol)
+);
+
+
 CREATE TABLE vol_avion(
    Id_avion INTEGER,
    Id_vol INTEGER,
-   date_heure_depart TIMESTAMP,
-   date_heure_arrive TIMESTAMP,
+   numero INTEGER,
    PRIMARY KEY(Id_avion, Id_vol),
    FOREIGN KEY(Id_avion) REFERENCES avion(Id_avion),
    FOREIGN KEY(Id_vol) REFERENCES vol(Id_vol)
 );
+
+-- Vue pour calculer le chiffre d'affaires par vol
+CREATE VIEW v_chiffre_affaires_vol AS
+SELECT 
+   v.Id_vol,
+   v.date_heure_depart,
+   v.date_heure_arrive,
+   v.prix_place,
+   mv.numero_vol,
+   COALESCE(SUM(CAST(ap.nombre_place AS INTEGER)), 0) AS total_places_vendues,
+   COALESCE(SUM(CAST(ap.nombre_place AS INTEGER) * v.prix_place), 0) AS chiffre_affaires
+FROM vol v
+JOIN modele_vol mv ON v.Id_modele_vol = mv.Id_modele_vol
+LEFT JOIN achat_places ap ON v.Id_vol = ap.Id_vol
+GROUP BY v.Id_vol, v.date_heure_depart, v.date_heure_arrive, v.prix_place, mv.numero_vol;
+
+-- Vue pour le chiffre d'affaires global
+CREATE VIEW v_chiffre_affaires_total AS
+SELECT 
+   SUM(chiffre_affaires) AS chiffre_affaires_total,
+   SUM(total_places_vendues) AS total_places_vendues
+FROM v_chiffre_affaires_vol;
--- database/data.sql
+++ database/data.sql
@@ -1,5 +1,42 @@
--- \i /home/antonio/ITU/S5/md-baovola/aerienne/database/data.sql
-insert into avions (modele, capacite) values
-('Boeing 737', 189),
-('Airbus A320', 180),
-('Embraer E190', 100);
+INSERT INTO aeroport (code_IATA, ville) VALUES
+('TNR', 'Antananarivo'),
+('DZA', 'Dzaoudzi'),
+('RUN', 'Saint-Denis'),
+('CDG', 'Paris'),
+('JNB', 'Johannesburg'),
+('NOS', 'Nosy Be');
+
+INSERT INTO avion (capacité, modele) VALUES
+(180, 'Boeing 737'),
+(220, 'Airbus A320'),
+(300, 'Boeing 787'),
+(150, 'ATR 72');
+
+INSERT INTO modele_vol (numero_vol, Id_aeroport_depart, Id_aeroport_arrive) VALUES
+('MD001', 1, 2),
+('MD002', 1, 3),
+('MD003', 4, 1),
+('MD004', 1, 5),
+('MD005', 1, 6);
+
+INSERT INTO vol (date_heure_depart, date_heure_arrive, prix_place, Id_modele_vol) VALUES
+('2026-02-01 08:00:00', '2026-02-01 10:30:00', 250000, 1),
+('2026-02-02 14:00:00', '2026-02-02 16:45:00', 350000, 2),
+('2026-02-03 21:00:00', '2026-02-04 08:00:00', 1500000, 3),
+('2026-02-05 09:30:00', '2026-02-05 13:00:00', 800000, 4),
+('2026-01-12 12:00:00', '2026-01-12 13:30:00', 450000, 5);
+
+
+INSERT INTO vol_avion (Id_avion, Id_vol, numero) VALUES
+(1, 1, 101),
+(2, 2, 202),
+(3, 3, 303),
+(4, 4, 404),
+(4, 5, 505);
+
+-- INSERT INTO achat_places (nom_client, nombre_place, Id_vol) VALUES
+-- ('Rakoto Jean', '2', 1),
+-- ('Rabe Marie', '1', 1),
+-- ('Smith John', '3', 2),
+-- ('Dupont Alice', '2', 3),
+-- ('Andry Paul', '4', 4);
